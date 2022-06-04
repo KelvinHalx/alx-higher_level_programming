b871c1c57ce97dd49f2ed0e099cf73ecb1d6fd9e
KelvinHalx <kelvinwainaina03@gmail.com>
2022-06-04 12:35:26 +0300

    Changes Result

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
-from add_0 import add
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a,b)
+    print(f"{a} + {b} = {result}")
